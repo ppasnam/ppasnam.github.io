122a452f40e4b194b7baf5e7ba9eb550cc85bb0f
Berat bayraktar <83634511+berat0123@users.noreply.github.com>
2022-02-19 21:33:24 +0200

    Add files via upload

---
+++ ------/messager.js
@@ -0,0 +1,24 @@
+function leave_a_message(){
+    console.log("Hey saddle if you know what you're doing now I would love for you to work with me.");
+    console.log("");
+    console.log("for communication;");
+    console.log("- bayraktarberat068@gmail.com");
+    console.log("- beratbayraktar693@gmail.com");
+    console.log("- bilemar@outlook.com");
+    console.log("");
+    console.log("I would love to meet with you");
+}
+
+function timer(){
+    setTimeout(leave_a_message, 10000);
+}
+
+timer()
+console.log("Hey saddle if you know what you're doing now I would love for you to work with me.");
+console.log("");
+console.log("for communication;");
+console.log("- bayraktarberat068@gmail.com");
+console.log("- beratbayraktar693@gmail.com");
+console.log("- bilemar@outlook.com");
+console.log("");
+console.log("I would love to meet with you");
---
+++ ------/readme.md
@@ -0,0 +1,8 @@
+Hey saddle if you know what you're doing now I would love for you to work with me.
+
+for communication;
+- bayraktarberat068@gmail.com
+- beratbayraktar693@gmail.com
+- bilemar@outlook.com
+
+I would love to meet with you
